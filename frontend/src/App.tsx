b874fd2e15bed91d96619408721eaf89538e188c
Paul Mayorga <jpmayorga@outlook.com>
2020-08-15 13:24:06 -0500

    feat(fb): adding environments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { PubllicationItem } from "./components/PublicationItem/PublicationItem";
 
 function App() {
   return (
     <div className="App">
+      <PubllicationItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ frontend/src/components/PublicationItem/PublicationItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+  background: "#eee";
+`;
+
+export function PubllicationItem() {
+  return <Header>Hola</Header>;
+}
