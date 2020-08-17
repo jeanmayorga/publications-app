e0f3b6d65eb38451bb082ede88f486988ccd2b5b
Paul Mayorga <jpmayorga@outlook.com>
2020-08-17 14:59:09 -0500

    fix(frontend): adding config test

--- frontend/src/config/index.test.ts
+++ frontend/src/config/index.test.ts
@@ -1,8 +1,10 @@
-import { render } from "@testing-library/react";
 import { BASE_API, environment } from ".";
 
 test("should get correct api backend", () => {
   if (environment === "production") {
     expect(BASE_API).toBe("https://publications-app.herokuapp.com");
+  }
+  if (environment === "development") {
+    expect(BASE_API).toBe("http://localhost:3000");
   }
 });
--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,6 +1,6 @@
 export let BASE_API = "http://localhost:3000";
 
-export const environment = process.env.REACT_APP_ENV;
+export const environment = process.env.REACT_APP_ENV ?? "production";
 console.log(`Environment: ${environment}`);
 
 if (environment === "production") {
